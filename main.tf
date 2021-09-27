22e3147e058e9f1d37a795efa3e37d626931f30f
taikinoda <taiki.noda@theport.jp>
2021-09-27 20:39:31 +0900

    3.2

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
+
+  tags = {
+    member = "noda",
+    usercase = "studying for terraform"
+  }
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
 }
