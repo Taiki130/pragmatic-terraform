335a0e200a4ba1905d295ca6a5911758d732cd61
taikinoda <taiki.noda@theport.jp>
2021-10-11 22:47:18 +0900

    operation

--- cloudwatch/aws_cloudwatch_log_group-for_ecs_scheduled_tasks.tf
+++ cloudwatch/aws_cloudwatch_log_group-for_ecs_scheduled_tasks.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "for_ecs" {
-  name              = "/ecs/example"
-  retention_in_days = 180
-}
-
 resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
   name              = "/ecs_scheduled-tasks/example"
   retention_in_days = 180
---
+++ operation/aws_iam_role.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+module "ec2_for_ssm_role" {
+  source     = "./iam_role"
+  name       = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ec2_for_ssm.json
+}
