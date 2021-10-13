3d416ac10bb89e2736ef30763c3156587a5ddddb
taikinoda <taiki.noda@theport.jp>
2021-10-14 00:26:28 +0900

    19.3 19.4

---
+++ example.tf
@@ -0,0 +1,42 @@
+# 19.3
+variable "allow_ssh" {
+  type = bool
+}
+
+resource "aws_security_group" "example" {
+  name = "example"
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_security_group_rule" "ingress" {
+  count = var.allow_ssh ? 1 : 0
+
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+output "allow_ssh_rule_id" {
+  value = join("", aws_security_group_rule.ingress[*].id)
+}
+
+# 19.4
+module "allow_ssh" {
+  source    = "./security_group"
+  allow_ssh = true
+}
+
+output "allow_ssh_rule_id" {
+  value = module.allow_ssh.allow_ssh_rule_id
+}
