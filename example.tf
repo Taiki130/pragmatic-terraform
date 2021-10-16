55a5a124eef1eaf63c6bfec0a58e5bc9deafa356
taikinoda <taiki.noda@theport.jp>
2021-10-16 15:13:31 +0900

    19.15

--- example.tf
+++ example.tf
@@ -135,3 +135,12 @@ output "virginia_vpc" {
 output "tokyo_vpc" {
   value = aws_vpc.tokyo.arn
 }
+
+# 19.15
+resource "aws_vpc" "default" {
+  cidr_block = "192.168.0.0/16"
+}
+
+output "vpc_arn" {
+  value = aws_vpc.default.arn
+}
