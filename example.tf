f6433f721dda7199ab387bcafa816e3332405250
taikinoda <taiki.noda@theport.jp>
2021-10-16 15:12:57 +0900

    19.14

--- example.tf
+++ example.tf
@@ -117,3 +117,21 @@ provider "aws" {
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+# 19.14
+resource "aws_vpc" "virginia" {
+  provider   = aws.virginia
+  cidr_block = "192.168.0.0/16"
+}
+
+resource "aws_vpc" "tokyo" {
+  cidr_block = "192.168.0.0/16"
+}
+
+output "virginia_vpc" {
+  value = aws_vpc.virginia.arn
+}
+
+output "tokyo_vpc" {
+  value = aws_vpc.tokyo.arn
+}
